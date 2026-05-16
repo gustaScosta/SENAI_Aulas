71f6bdeca164e8b85b48ff3c65d646db450cc71f
gustaScosta <166058957+gustaScosta@users.noreply.github.com>
2026-05-16 17:50:36 -0300

    Update aula.py

--- Aula 18 - Pacotes e Frameworks/aula.py
+++ Aula 18 - Pacotes e Frameworks/aula.py
@@ -1,22 +1,29 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 
 st.title('Aula 18 - Pacotes e Frameworks')
 
 
 class livro():
-    def __init__(self, titulo, autor, ano):
+    def __init__(self, titulo, autor, data_publicacao):
         self.titulo = titulo
         self.autor = autor
-        self.ano = ano
+        self.data_publicacao = data_publicacao
 
     def cadastroar_livro(self):
-        st.write(f"Livro cadastrado: {self.titulo} - {self.autor} ({self.ano})")
-        return self.titulo, self.autor, self.ano
-    
+        data_formatada = self.data_publicacao.strftime("%d/%m/%Y")
+        st.write(f"Livro cadastrado: {self.titulo} - {self.autor} ({data_formatada})")
+        return self.titulo, self.autor, self.data_publicacao
+
+
 nome = st.text_input("Digite o nome do livro:")
 autor = st.text_input("Digite o nome do autor:")
-ano = st.number_input("Digite o ano de publicação:", min_value=0, max_value=2026, step=1)
+data_publicacao = st.date_input(
+    "Digite a data de publicacao:",
+    value=pd.Timestamp("2023-01-01"),
+    format="DD/MM/YYYY"
+)
+
 if st.button("Cadastrar Livro"):
-    livro_cadastrado = livro(nome, autor, ano)
+    livro_cadastrado = livro(nome, autor, data_publicacao)
     livro_cadastrado.cadastroar_livro()
