5fade2b23eb6f52e4e7a81c58cf92b2a6ecdb301
gustaScosta <166058957+gustaScosta@users.noreply.github.com>
2026-05-23 11:43:38 -0300

    Update aula.py

---
+++ Aula 16 - Polimorfismo/aula.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+class portifolio:
+    def __init__(self):
+        name = st.title('gustavo')
+        link = st.write('https')
+        whats = st.write('(11) 95172 - 8785')
+        email = st.success('Rua jorge garcia de medeiros')
+
+    # def display():
+    #     img = st.image('')
+    #     audio = st.audio('')
+
+ob = portifolio()
